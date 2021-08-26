a0eba96ccf86d3588e1898e6d0540a9812c06954
charlie <charlie.tang@accenture.com>
2021-08-26 00:32:33 -0400

    started on the items info page

--- components/navbar.js
+++ components/navbar.js
@@ -6,8 +6,8 @@ class Navbar extends React.Component {
             <nav className="w-full md:pt-0 px-6 shadow-lg relative z-20 border-t border-b border-gray-400 bg-Antique-Brass">
                 <div className="container mx-auto md:flex justify-between items-center text-sm md:text-md md:justify-start">
                     <div className="w-full md:w-1/2 text-center md:text-left py-2 flex flex-wrap justify-center items-stretch md:justify-start md:items-start">
-                        <a href="#" className="px-2 md:pl-0 md:mr-3 md:pr-3 text-gray-700 no-underline md:border-r border-gray-400">Home</a>
-                        <a href="#" className="px-2 md:pl-0 md:mr-3 md:pr-3 text-gray-700 no-underline md:border-r border-gray-400">Prices</a>
+                        <a href="http://localhost:3000" className="px-2 md:pl-0 md:mr-3 md:pr-3 text-gray-700 no-underline md:border-r border-black">Home</a>
+                        <a href="http://localhost:3000/prices/items" className="px-2 md:pl-0 md:mr-3 md:pr-3 text-gray-700 no-underline md:border-r border-black">Prices</a>
                         <a href="#" className="px-2 md:pl-0 md:mr-3 md:pr-3 text-gray-700 no-underline">Listings</a>
                     </div>
                     <div className="w-full md:w-1/2 text-center md:text-right pb-4 md:p-0">
---
+++ pages/prices/items/index.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "../../../components/navbar";
+import Header from "../../../components/header";
+import Footer from "../../../components/footer";
+import Datagrid from "../../../components/datagrid";
+import Popularitems from "../../../popitems"
+import Head from 'next/head'
+import 'tailwindcss/tailwind.css'
+
+
+class WelcomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: [],
+        };
+    }
+    componentDidMount() {
+        fetch("http://localhost:4000/api/test", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        })
+            .then((response)=>{
+                return response.json()}) // RETURN RESPONSE.JSON()
+            .then(
+                data=> {
+                    // console.log(typeof data);
+                    this.setState({
+                        isLoaded: true,
+                        items: Object.values(data),
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error,
+                    });
+                }
+            );
+    }
+    render() {
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Error:{error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div className="h-screen">
+                    <Head>
+                        <title>Maple Market</title>
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                        <meta name="description" content="Prices for Maplestory"></meta>
+                        <meta name="keywords" content="Maplestory, GMS, EMS"></meta>
+                        <meta charset="UTF-8"></meta>
+                    </Head>
+                    <Header />
+                    <Navbar />
+                    <h1 className="text-center py-2 bg-Artichoke"> Welcome to MapleMarket! </h1>
+                    <Popularitems items={items} />
+                    <Footer />
+                </div>
+            );
+        }
+    }
+}
+
+export default WelcomePage;
