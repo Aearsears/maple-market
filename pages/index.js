04aaef8a628fc3095c3aa7b7fcb7eccd34b523a8
Aearsears <53102185+Aearsears@users.noreply.github.com>
2021-09-02 23:35:27 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Navbar from "../components/navbar";
 import Header from "../components/header";
 import Footer from "../components/footer";
