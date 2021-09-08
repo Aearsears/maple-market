6bb002050a353c451c9b55ac6a12c2dd42d05c5b
Aearsears <53102185+Aearsears@users.noreply.github.com>
2021-09-07 21:44:50 -0400

    trying out netlify redirects

--- next.config.js
+++ next.config.js
@@ -10,11 +10,11 @@ module.exports = {
         destination: '/prices',
         permanent: true,
       },
-      {
-        source:'/api/:slug',
-        destination:'https://maple-market-db.herokuapp.com/:slug',
-        permanent: true,
-      }
+      // {
+      //   source:'/api/:slug',
+      //   destination:'https://maple-market-db.herokuapp.com/:slug',
+      //   permanent: true,
+      // }
     ]
   },
 };
